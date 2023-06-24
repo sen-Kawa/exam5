f80490f6306ba72b55a9d7b24b7534ecd83e8a74
olin_tp_ar <universalexperimental@protonmail.com>
2023-06-24 17:42:18 +0200

    skills warlock

--- t/Warlock.cpp
+++ t/Warlock.cpp
@@ -1,5 +1,26 @@
 #include "Warlock.hpp"
 
+void Warlock::learnSpell(ASpell* newSpell)
+{
+	if (spells.count(newSpell->getName()) == 0)
+		spells[newSpell->getName()] = newSpell->clone();
+}
+
+void Warlock::forgetSpell(std::string nameSpell)
+{
+	if (spells.count(nameSpell) > 0)
+	{
+		delete spells[nameSpell];
+		spells.erase(nameSpell);
+	}
+}
+
+void Warlock::launchSpell(std::string nameSpell, ATarget& target)
+{
+	if (spells.count(nameSpell) > 0)
+		spells[nameSpell]->launch(target);
+}
+
 std::string const& Warlock::getName() const
 {
 	return (name);
--- t/Warlock.hpp
+++ t/Warlock.hpp
@@ -3,13 +3,18 @@
 #pragma once
 #include <ios>
 #include <iostream>
-
+#include <map>
+#include "ATarget.hpp"
+#include "ASpell.hpp"
+class ATarget;
+class ASpell;
 class Warlock
 {
 	private:
 
 		std::string name;
 		std::string title;
+		std::map<std::string, ASpell*> spells;
 
 		Warlock();
 		Warlock(Warlock const& obj);
@@ -21,6 +26,9 @@ class Warlock
 		std::string const& getTitle() const;
 		void setTitle(std::string const& newTitle);
 		void introduce() const;
+		void learnSpell(ASpell* newSpell);
+		void forgetSpell(std::string nameSpell);
+		void launchSpell(std::string nameSpell, ATarget&);
 
 		Warlock(std::string const name, std::string const title);
 		~Warlock();
